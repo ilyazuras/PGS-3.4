464c1886b792dff691da70b6d226342737016631
Syth <feraogfn@gmail.com>
2025-10-22 17:55:53 +1100

    Update looting.cpp

--- 3.4/Private/looting.cpp
+++ 3.4/Private/looting.cpp
@@ -1,61 +1,63 @@
 #include "pch.h"
-#include "Inventory.h"
+#include "Looting.h"
 #include "Utils.h"
+#include "Inventory.h"
 
-UFortItemDefinition* Inventory::GiveItem(AFortPlayerControllerAthena* Controller,UFortItemDefinition* ItemDef,int Count,int LoadedAmmo,int Slot,bool Equip,bool Update,int EntryIndex)
+FItemAndCount Looting::GetWeapons()
 {
- if(!Controller||!ItemDef)return nullptr;
- auto World=UWorld::GetWorld();
- auto Pawn=(AFortPlayerPawnAthena*)Controller->Pawn;
- if(!Pawn)return nullptr;
- auto Inventory=Controller->WorldInventory;
- if(!Inventory)return nullptr;
- FFortItemEntry Entry{};
- Entry.Count=Count;
- Entry.ItemDefinition=ItemDef;
- Entry.LoadedAmmo=LoadedAmmo;
- Entry.Durability=1.f;
- EntryEntryGuid=FGuid::NewGuid();
- Inventory->Inventory.ReplicatedEntries.Add(Entry);
- if(Update)Inventory->HandleInventoryLocalUpdate();
- if(Equip)Pawn->EquipWeapon(ItemDef);
- Inventory->ForceNetUpdate();
- return ItemDef;
+ static UEAllocatedVector<FItemAndCount> Weapons{
+  FItemAndCount(1,{},Utils::FindObject<UFortWeaponRangedItemDefinition>(L"/Game/Athena/Items/Weapons/WID_Assault_AutoHigh_Athena_R_Ore_T03.WID_Assault_AutoHigh_Athena_R_Ore_T03")),
+  FItemAndCount(1,{},Utils::FindObject<UFortWeaponRangedItemDefinition>(L"/Game/Athena/Items/Weapons/WID_Shotgun_Standard_Athena_R_Ore_T03.WID_Shotgun_Standard_Athena_R_Ore_T03")),
+  FItemAndCount(1,{},Utils::FindObject<UFortWeaponRangedItemDefinition>(L"/Game/Athena/Items/Weapons/WID_Sniper_BoltAction_Scope_Athena_R_Ore_T03.WID_Sniper_BoltAction_Scope_Athena_R_Ore_T03")),
+  FItemAndCount(1,{},Utils::FindObject<UFortWeaponRangedItemDefinition>(L"/Game/Athena/Items/Weapons/WID_Pistol_SemiAuto_Athena_R_Ore_T03.WID_Pistol_SemiAuto_Athena_R_Ore_T03"))
+ };
+ return Weapons[rand()%(Weapons.size()-1)];
 }
 
-void Inventory::ClearInventory(AFortPlayerControllerAthena* Controller)
+FItemAndCount Looting::GetConsumables()
+{
+ static UEAllocatedVector<FItemAndCount> Consumables{
+  FItemAndCount(3,{},Utils::FindObject<UFortConsumableItemDefinition>(L"/Game/Athena/Items/Consumables/Athena_Shields.Athena_Shields")),
+  FItemAndCount(6,{},Utils::FindObject<UFortConsumableItemDefinition>(L"/Game/Athena/Items/Consumables/Athena_ShieldSmall.Athena_ShieldSmall")),
+  FItemAndCount(3,{},Utils::FindObject<UFortConsumableItemDefinition>(L"/Game/Athena/Items/Consumables/Athena_Bandage.Athena_Bandage")),
+  FItemAndCount(2,{},Utils::FindObject<UFortConsumableItemDefinition>(L"/Game/Athena/Items/Consumables/Athena_Medkit.Athena_Medkit"))
+ };
+ return Consumables[rand()%(Consumables.size()-1)];
+}
+
+UFortAmmoItemDefinition* Looting::GetAmmo(EAmmoType AmmoType)
+{
+ static UEAllocatedVector<UFortAmmoItemDefinition*> Ammos{
+  Utils::FindObject<UFortAmmoItemDefinition>(L"/Game/Athena/Items/Ammo/AthenaAmmoDataBulletsLight.AthenaAmmoDataBulletsLight"),
+  Utils::FindObject<UFortAmmoItemDefinition>(L"/Game/Athena/Items/Ammo/AthenaAmmoDataShells.AthenaAmmoDataShells"),
+  Utils::FindObject<UFortAmmoItemDefinition>(L"/Game/Athena/Items/Ammo/AthenaAmmoDataBulletsMedium.AthenaAmmoDataBulletsMedium"),
+  Utils::FindObject<UFortAmmoItemDefinition>(L"/Game/Athena/Items/Ammo/AthenaAmmoDataBulletsHeavy.AthenaAmmoDataBulletsHeavy"),
+  Utils::FindObject<UFortAmmoItemDefinition>(L"/Game/Athena/Items/Ammo/AmmoDataRockets.AmmoDataRockets")
+ };
+ return Ammos[(uint8)AmmoType];
+}
+
+UFortResourceItemDefinition* Looting::GetResource(EFortResourceType ResourceType)
+{
+ static UEAllocatedVector<UFortResourceItemDefinition*> Resources{
+  Utils::FindObject<UFortResourceItemDefinition>(L"/Game/Items/ResourcePickups/WoodItemData.WoodItemData"),
+  Utils::FindObject<UFortResourceItemDefinition>(L"/Game/Items/ResourcePickups/StoneItemData.StoneItemData"),
+  Utils::FindObject<UFortResourceItemDefinition>(L"/Game/Items/ResourcePickups/MetalItemData.MetalItemData")
+ };
+ return Resources[(uint8)ResourceType];
+}
+
+void Looting::GiveDefaultLoot(AFortPlayerControllerAthena* Controller)
 {
  if(!Controller)return;
- auto Inventory=Controller->WorldInventory;
- if(!Inventory)return;
- Inventory->Inventory.ReplicatedEntries.Clear();
- Inventory->HandleInventoryLocalUpdate();
- Inventory->ForceNetUpdate();
-}
-
-void Inventory::RemoveItem(AFortPlayerControllerAthena* Controller,UFortItemDefinition* ItemDef)
-{
- if(!Controller||!ItemDef)return;
- auto Inventory=Controller->WorldInventory;
- if(!Inventory)return;
- for(int i=0;i<Inventory->Inventory.ReplicatedEntries.Num();i++)
- {
-  if(Inventory->Inventory.ReplicatedEntries[i].ItemDefinition==ItemDef)
-  {
-   Inventory->Inventory.ReplicatedEntries.RemoveAt(i);
-   break;
-  }
- }
- Inventory->HandleInventoryLocalUpdate();
- Inventory->ForceNetUpdate();
-}
-
-void Inventory::HandleInventoryLocalUpdate(AFortPlayerControllerAthena* Controller)
-{
- if(!Controller)return;
- auto Inventory=Controller->WorldInventory;
- if(!Inventory)return;
- static void(*HandleLocalUpdate)(UFortInventory*)=decltype(HandleLocalUpdate)(ImageBase+0x5D47B30);
- HandleLocalUpdate(Inventory);
- Inventory->ForceNetUpdate();
+ Inventory::ClearInventory(Controller);
+ auto Weapon=GetWeapons().ItemDefinition;
+ Inventory::GiveItem(Controller,Weapon,1,0,1,true,true,0);
+ auto Heal=GetConsumables().ItemDefinition;
+ Inventory::GiveItem(Controller,Heal,3,0,2,false,true,0);
+ Inventory::GiveItem(Controller,(UFortItemDefinition*)GetResource(EFortResourceType::Wood),999,0,3,false,true,0);
+ Inventory::GiveItem(Controller,(UFortItemDefinition*)GetResource(EFortResourceType::Stone),999,0,4,false,true,0);
+ Inventory::GiveItem(Controller,(UFortItemDefinition*)GetResource(EFortResourceType::Metal),999,0,5,false,true,0);
+ Controller->WorldInventory->HandleInventoryLocalUpdate();
+ Controller->WorldInventory->ForceNetUpdate();
 }
